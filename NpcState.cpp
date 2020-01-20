2a598b6574f326b4ce5d5dae4b800cbb2a5f0ef1
Jiye <38411987+jiyemoon@users.noreply.github.com>
2020-01-20 09:54:04 +0900

    NpsState 가상함수 추가

--- NpcState.cpp
+++ NpcState.cpp
@@ -1,2 +1,18 @@
 #include "stdafx.h"
 #include "NpcState.h"
+
+void NpcState::Idle(Npc* npc)
+{
+}
+
+void NpcState::Search(Npc* npc)
+{
+}
+
+void NpcState::Inline(Npc* npc)
+{
+}
+
+void NpcState::GoHome(Npc* npc)
+{
+}
--- NpcState.h
+++ NpcState.h
@@ -7,22 +7,32 @@ private:
 
 
 
-public:
-
-
+protected:
 
 	//setter
-	void SetNpcState(Npc* npc, NPCSTATE* npcstate) 
+	void SetNpcState(Npc* npc, NpcState* npcstate)
 	{ 
 		npc->SetState(npcstate);
 	}
 
+public :
+	virtual void Idle(Npc* npc) = 0;
+	virtual void Search(Npc* npc) = 0;
+	virtual void Inline(Npc* npc) = 0;
+	virtual void GoHome(Npc* npc) = 0;
 
 };
 
 class NpcStateIdle : NpcState
 {
+	//Npc°¡ ¾ê ½á¾ßÇØ¼­ ½ºÅÂÆ½..
+	static NpcStateIdle* instance;
 
+public:
+	virtual void Idle(Npc* npc);
+	virtual void Search(Npc* npc);
+	virtual void Inline(Npc* npc);
+	virtual void GoHome(Npc* npc);
 };
 class NpcStateSearch : NpcState
 {
