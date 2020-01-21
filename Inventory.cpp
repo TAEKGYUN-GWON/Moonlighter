fb3dec4484f1773676689dac2e9941edbc13b3cd
TAEKGYUN-GWON <god_lion@naver.com>
2020-01-21 10:26:36 +0900

    인벤토리 시작

--- Inventory.cpp
+++ Inventory.cpp
@@ -10,3 +10,24 @@ Inventory::Inventory()
 Inventory::~Inventory()
 {
 }
+
+void Inventory::Init()
+{
+}
+
+void Inventory::Update()
+{
+}
+
+void Inventory::Release()
+{
+}
+
+void Inventory::Insert(Item* item)
+{
+	
+}
+
+void Inventory::Remove(string name)
+{
+}
--- Inventory.h
+++ Inventory.h
@@ -3,10 +3,25 @@
 class Inventory
 {
 private:
-	vector<Item*> _itemList;
+	
+	struct tagItemInfo
+	{
+		Item* item;
+		int some;
+	};
+	multimap<string, tagItemInfo> _inven;
+	multimap<string, tagItemInfo>::iterator iter;
 
 
 public:
 	Inventory();
 	~Inventory();
+
+	void Init();
+	void Update();
+	void Release();
+
+	void Insert(Item* item);
+	void Remove(string name);
+
 };
