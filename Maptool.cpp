8085d539d83b6fa2cb897339aedc2d0ea5b1a1a7
TAEKGYUN-GWON <god_lion@naver.com>
2020-01-22 23:34:21 +0900

    던전 타일 이미지 추가

--- Maptool.cpp
+++ Maptool.cpp
@@ -36,7 +36,25 @@ void Maptool::Init()
 	GRAPHICMANAGER->AddImage("empty", L"resource/img/empty.png");
 #pragma endregion
 
+#pragma region SetDungeonSet
+	GRAPHICMANAGER->AddImage("bigRock", L"resource/img/Object/bigRock.png");
+	GRAPHICMANAGER->AddImage("rock", L"resource/img/Object/rock.png");
+	GRAPHICMANAGER->AddImage("brokenPillar", L"resource/img/Object/brokenPillar.png");
+	GRAPHICMANAGER->AddImage("fountain", L"resource/img/Object/fountain.png");
+	GRAPHICMANAGER->AddImage("lathe", L"resource/img/Object/lathe.png");
+	GRAPHICMANAGER->AddImage("pillar", L"resource/img/Object/pillar.png");
+	GRAPHICMANAGER->AddImage("pot", L"resource/img/Object/pot.png");
+	GRAPHICMANAGER->AddImage("pot_slime", L"resource/img/Object/pot_slime.png");
+	GRAPHICMANAGER->AddImage("skull1", L"resource/img/Object/skull1.png");
+	GRAPHICMANAGER->AddImage("skull2", L"resource/img/Object/skull2.png");
+	GRAPHICMANAGER->AddImage("skull3", L"resource/img/Object/skull3.png");
+	GRAPHICMANAGER->AddImage("smallRock", L"resource/img/Object/smallRock.png");
+	GRAPHICMANAGER->AddImage("smallRock_slime", L"resource/img/Object/smallRock_slime.png");
+
+#pragma endregion
+
 	GRAPHICMANAGER->AddFrameImage("set_tile", L"set_tile3.png", 4, 6);
+	GRAPHICMANAGER->AddFrameImage("set_tile_dungeon", L"set_tile_dungeon.png", 4, 6);
 	GRAPHICMANAGER->AddFrameImage("will_dungeon", L"will_dungeon.png", 10, 13);
 
 	SetUp();
@@ -264,7 +282,6 @@ void Maptool::SetUp()
 
 	_mTileList.insert(make_pair(("forgeBoard"), tagTile().Clone("forgeBoard", "Wall", false, 1, 1, PIVOT::RIGHT_BOTTOM, Vector2(2, 1), Vector2(2, 1))));
 	_mTileList.insert(make_pair(("potionBoard"), tagTile().Clone("potionBoard", "Wall", false, 1, 1, PIVOT::RIGHT_BOTTOM, Vector2(2, 1), Vector2(2, 1))));
-
 	_mTileList.insert(make_pair(("empty"), tagTile().Clone("empty", "None", false, 1, 1, PIVOT::RIGHT_BOTTOM, Vector2(1, 1), Vector2(1, 1))));
 
 	_sampleTile[0].imgKey = _mTileList.find("build_fountain")->second->imgKey;
