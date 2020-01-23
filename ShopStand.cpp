2a3ba6c6321253c93230ba7f3740fb4d0993c11f
Jiye <38411987+jiyemoon@users.noreply.github.com>
2020-01-23 09:16:51 +0900

    ShopStandManager 추가

--- ShopStand.cpp
+++ ShopStand.cpp
@@ -3,6 +3,11 @@
 
 void ShopStand::Init()
 {
+	_tag = "ShopObject";
+	_name = "ShopStand";
+
+	_itemPos = Vector2(0, 0); //나중에 바꿔줘야함 데스크 좌표의 가운데로
+	_itemImgName = ""; //이거 어떡함 아이템 어떻게 관리할지 모르겠음
 }
 
 void ShopStand::Release()
