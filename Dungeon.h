f6ef95c4d3e58238ffb0b33ea34c4d2197b35e17
TAEKGYUN-GWON <god_lion@naver.com>
2020-01-27 19:54:07 +0900

    던전 최적화중

--- Dungeon.h
+++ Dungeon.h
@@ -11,10 +11,13 @@ class Dungeon : public Scene
 {
 private:
 	Tile* _tiles[Dungeon_X * Dungeon_Y];
+	vector<Tile*> tiles;
 	tagTile _tagTiles[Dungeon_X * Dungeon_Y];
 	EnemyManeger* _eMgr;
+	Vector2 pos;
+	bool _isAllowInit = false;
 public:
-	virtual void Init();
+	virtual void Init(Vector2 start);
 	virtual void Update();
 	virtual void Render();
 	void SetUp();
