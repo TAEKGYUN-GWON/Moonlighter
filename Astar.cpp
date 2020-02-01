23ee31603174b108720a548dac5821b53f25e202
TAEKGYUN-GWON <god_lion@naver.com>
2020-02-01 21:27:32 +0900

    A스타 변경

--- Astar.cpp
+++ Astar.cpp
@@ -52,7 +52,7 @@ void Astar::InitTotalList()
 	//}
 	for (Tile* t : _vTotalList)
 	{
-		if (t->GetAttribute() == "Wall")continue;
+		if (t->GetAttribute() == "Wall"|| t->GetAttribute() == "NpcNone")continue;
 		t->SetCostF(-1);
 		t->SetCostG(0);
 		t->SetCostH(0);
@@ -111,31 +111,31 @@ vector <Tile*> Astar::GetDirList(Vector2 idx)
 	nodeList.clear();
 
 
-	////_miTotalList = _mTotalList.find(Vector2(idx.x+ (int)1, idx.y + (int)1));
-	//if (CanOpenRight(idx) && CanOpenDown(idx))
-	//	nodeList.push_back(_vTotalList[((int)idx.x + maxX * (int)idx.y) + 1 + maxX]);
+	//_miTotalList = _mTotalList.find(Vector2(idx.x+ (int)1, idx.y + (int)1));
+	if (CanOpenRight(idx) && CanOpenDown(idx))
+		nodeList.push_back(_vTotalList[((int)idx.x + maxX * (int)idx.y) + 1 + maxX]);
 
 
-	////_miTotalList = _mTotalList.find(Vector2(idx.x - (int)1, idx.y + (int)1));
-	//if (CanOpenLeft(idx) && CanOpenDown(idx))
-	//	nodeList.push_back(_vTotalList[((int)idx.x + maxX * (int)idx.y) - 1 + maxX]);
+	//_miTotalList = _mTotalList.find(Vector2(idx.x - (int)1, idx.y + (int)1));
+	if (CanOpenLeft(idx) && CanOpenDown(idx))
+		nodeList.push_back(_vTotalList[((int)idx.x + maxX * (int)idx.y) - 1 + maxX]);
 
 
-	////_miTotalList = _mTotalList.find(Vector2(idx.x + (int)1, idx.y - (int)1));
-	//if (CanOpenRight(idx) && CanOpenUp(idx))
-	//	nodeList.push_back(_vTotalList[((int)idx.x + maxX * (int)idx.y) + 1 - maxX]);
+	//_miTotalList = _mTotalList.find(Vector2(idx.x + (int)1, idx.y - (int)1));
+	if (CanOpenRight(idx) && CanOpenUp(idx))
+		nodeList.push_back(_vTotalList[((int)idx.x + maxX * (int)idx.y) + 1 - maxX]);
 
 
-	////_miTotalList = _mTotalList.find(Vector2(idx.x - (int)1, idx.y - (int)1));
-	//if (CanOpenLeft(idx) && CanOpenUp(idx))
-	//	nodeList.push_back(_vTotalList[((int)idx.x + maxX * (int)idx.y) - 1 - maxX]);
+	//_miTotalList = _mTotalList.find(Vector2(idx.x - (int)1, idx.y - (int)1));
+	if (CanOpenLeft(idx) && CanOpenUp(idx))
+		nodeList.push_back(_vTotalList[((int)idx.x + maxX * (int)idx.y) - 1 - maxX]);
 
-	//for (Tile* t : nodeList)
-	//{
-	//	//_miTotalList = _mTotalList.find(Vector2(idx.x, idx.y));
-	//	if (SetCost(t, 14, _vTotalList[((int)idx.x + maxX * (int)idx.y)]))
-	//		dirList.push_back(t);
-	//}
+	for (Tile* t : nodeList)
+	{
+		//_miTotalList = _mTotalList.find(Vector2(idx.x, idx.y));
+		if (SetCost(t, 14, _vTotalList[((int)idx.x + maxX * (int)idx.y)]))
+			dirList.push_back(t);
+	}
 
 	return dirList;
 }
