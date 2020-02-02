25815814ec096c407ef9fd6d46039512c45a73d8
Jiye <38411987+jiyemoon@users.noreply.github.com>
2020-02-03 02:27:04 +0900

    판매대로 가지를 않음

--- Npc.h
+++ Npc.h
@@ -4,7 +4,7 @@
 #include "ShopStandManager.h"
 #include "CheckStand.h"
 #include "Sprite.h"
-#define NPCSPEED 70
+#define NPDSPEED 150
 
 class ShopStandManager;
 class CheckStand;
@@ -13,91 +13,92 @@ class NpcShopState;
 //이제 어디로 가고싶은지(destination이랑 다름)
 enum NPCTHOUGHT
 {
-	WINDOW,//창문에 가고싶음
-	CHOOSE,
-	GOHOME, //집에가고싶음
-	BUY, //사고싶음(줄서고싶음)
-	NOTBUY, //안사고싶음
-	END
+    WINDOW,//창문에 가고싶음
+    CHOOSE,
+    GOHOME, //집에가고싶음
+    BUY, //사고싶음(줄서고싶음)
+    NOTBUY, //안사고싶음
+    END
 };
 
 //현재 어디 있는지
 enum NPCNOWPOSITION
 {
-	POS_ENTER,
-	POS_WINDOW,
-	POS_STAND1,
-	POS_STAND2,
-	POS_STAND3,
-	POS_STAND4,
-	POS_CHECKSTAND,
-	POS_DOOR,
-	POS_END
+    POS_ENTER,
+    POS_WINDOW,
+    POS_STAND1,
+    POS_STAND2,
+    POS_STAND3,
+    POS_STAND4,
+    POS_CHECKSTAND,
+    POS_DOOR,
+    POS_END
 };
 
-class Npc :	public Object
+class Npc : public Object
 {
 private:
-	Sprite* _sprite;
+    Sprite* _sprite;
 
-	NPCTHOUGHT _npcThought; //머해야할지..
-	NPCNOWPOSITION _npcNowPosition;
+    NPCTHOUGHT _npcThought; //머해야할지..
+    NPCNOWPOSITION _npcNowPosition;
 
-	float _timer;
+    float _timer;
 
-	//float _speed;
-	bool _isShopSOn;	//가판대랑 충돌중인지
-	bool _isCheckSOn;	//계산대랑 충돌중인지
+    //float _speed;
+    bool _isShopSOn;   //가판대랑 충돌중인지
+    bool _isCheckSOn;   //계산대랑 충돌중인지
 
-	bool _isAstarOn;	//astar 받아야하는 상태인지
+    bool _isAstarOn;   //astar 받아야하는 상태인지
 
-	NpcShopState* _npcShopState;
+    NpcShopState* _npcShopState;
 
-	//ShopStand* _shopStand; //스탠드를 가져야될지 매니저를 가져야될지 모르겠음
-	ShopStandManager* _shopStandMgr;
-	CheckStand* _checkStand;
+    //ShopStand* _shopStand; //스탠드를 가져야될지 매니저를 가져야될지 모르겠음
+    ShopStandManager* _shopStandMgr;
+    CheckStand* _checkStand;
 
-	list<Vector2> _lPath; //가야하는 길
+    list<Vector2> _lPath; //가야하는 길
 
 public:
-	Npc() {};
-	~Npc() {};
+    Npc() {};
+    ~Npc() {};
 
-	virtual void Init(string imgkey, Vector2 pos);
-	virtual void Release() override;
-	virtual void Update() override;
-	virtual void Render() override;
+    virtual void Init(string imgkey, Vector2 pos);
+    virtual void Release() override;
+    virtual void Update() override;
+    virtual void Render() override;
 
-	void SetPath(list<Vector2> lpath);
-	void Move();
-	
-	//상태 정의용
-	void ChangeState(NpcShopState* state);
+    void SetPath(list<Vector2> lpath);
+    void Move();
 
-	//계산대/판매대 링크용
-	void SetCheckStandLink(CheckStand* checkstand) { _checkStand = checkstand; }
-	CheckStand* GetCheckStand() { return _checkStand; }
-	void SetShopStandLink(ShopStandManager* shopstand) { _shopStandMgr = shopstand; }
-	ShopStandManager* GetShopStandMgr() { return _shopStandMgr; }
+    NpcShopState* GetNpcState() { return _npcShopState; }
 
-	//계산대랑 가판대랑 충돌중ING인지 체크할 bool
-	void SetIsShopSOn(bool shop) { _isShopSOn = shop; }
-	bool GetIsShopSOn() { return _isShopSOn; }
-	void SetIsCheckSOn(bool check) { _isCheckSOn = check; }
-	bool GetIsCheckSOn() { return _isCheckSOn; }
-	void SetIsAstarOn(bool ast) { _isAstarOn = ast; }
-	bool GetIsAstarOn() { return _isAstarOn; }
+    //상태 정의용
+    void ChangeState(NpcShopState* state);
 
-	//NPC 이제 뭐하고싶은 상태인지..
-	void SetNpcThought(NPCTHOUGHT npcthought) { _npcThought = npcthought; }
-	NPCTHOUGHT GetNpcThought() { return _npcThought; }
-	//위치 뭔지
-	void SetNpcNowPosition(NPCNOWPOSITION npcnowposition) { _npcNowPosition = npcnowposition; }
-	NPCNOWPOSITION GetNpcNowPosition() { return _npcNowPosition; }
+    //계산대/판매대 링크용
+    void SetCheckStandLink(CheckStand* checkstand) { _checkStand = checkstand; }
+    CheckStand* GetCheckStand() { return _checkStand; }
+    void SetShopStandLink(ShopStandManager* shopstand) { _shopStandMgr = shopstand; }
+    ShopStandManager* GetShopStandMgr() { return _shopStandMgr; }
 
-	NpcShopState* GetStatePointer() { return _npcShopState; }
-	list<Vector2>* GetPath() { return &_lPath; }
+    //계산대랑 가판대랑 충돌중ING인지 체크할 bool
+    void SetIsShopSOn(bool shop) { _isShopSOn = shop; }
+    bool GetIsShopSOn() { return _isShopSOn; }
+    void SetIsCheckSOn(bool check) { _isCheckSOn = check; }
+    bool GetIsCheckSOn() { return _isCheckSOn; }
+    void SetIsAstarOn(bool ast) { _isAstarOn = ast; }
+    bool GetIsAstarOn() { return _isAstarOn; }
+
+    //NPC 이제 뭐하고싶은 상태인지..
+    void SetNpcThought(NPCTHOUGHT npcthought) { _npcThought = npcthought; }
+    NPCTHOUGHT GetNpcThought() { return _npcThought; }
+    //위치 뭔지
+    void SetNpcNowPosition(NPCNOWPOSITION npcnowposition) { _npcNowPosition = npcnowposition; }
+    NPCNOWPOSITION GetNpcNowPosition() { return _npcNowPosition; }
+
+    NpcShopState* GetStatePointer() { return _npcShopState; }
+    list<Vector2>* GetPath() { return &_lPath; }
 
 
 };
-
